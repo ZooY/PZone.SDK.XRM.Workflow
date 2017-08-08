0db27746bb0cbc55b1daed3976406e6f1b4634c3
Roman Kopaev <roman@kopaev.ru>
2017-08-08 11:50:09 +0300

    no message

---
+++ Source/PZone.Xrm.Workflow/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PZone.Xrm.Workflow")]
+[assembly: AssemblyDescription("Common assembly for Microsoft Dynamics Workflow development.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Programming Zone")]
+[assembly: AssemblyProduct("PZone SDK XRM")]
+[assembly: AssemblyCopyright("Copyright © 2017 Programming Zone")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ead18d9-4522-4ce0-b4b7-eedb1cd59403")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
